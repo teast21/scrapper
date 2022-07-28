64033ab95691e1981f755fcd61940ad81717a7a1
teast21 <109384972+teast21@users.noreply.github.com>
2022-07-28 10:45:02 -0400

    Update scrapper.go
    works on proper use case still need to add file import

--- scrapper.go
+++ scrapper.go
@@ -8,10 +8,10 @@ import (
 )
 
 type Deposit struct {
-	Pubkey string
-	//withdrawal_credentials string
-	//signature              string
-	//deposit_data_root      string
+	Pubkey                 string
+	Withdrawal_credentials string
+	Signature              string
+	Deposit_data_root      string
 }
 
 func main() {
@@ -19,17 +19,17 @@ func main() {
 	var deposit []Deposit
 
 	Data := []byte(`
-    [{"pubkey": "b06a9e28251e0402af1a7249e6be0f27f1eb986ad8fa00e44619efe0a3b963f9302fe109bc198bdb7f279ced3fcdb652", 
-    "withdrawal_credentials": "008cab8f5ba17523d30c8e5d48b0dae637d869e7bb9f5e278f4a710b9634ca3e", 
+    [{"pubkey": "pk1", 
+    "withdrawal_credentials": "wc1", 
     "amount": 32000000000, "signature": 
-    "abaf385e5814d2c852f038352696943d1f61931195724d934ebfedf561b8709cbb05cf2c465b74029f02a8680c5f5b8d091759bb241fff35c0479a9613f58ae8b86d77a738e9373f5f808d8f8d70f6588ca71c3c1182695ee3402e9a3686278c", "deposit_message_root": "afa202331b64e084dfd6f136cd3d63bc43e1c7e37b3135f02e260facb003bf17", 
-    "deposit_data_root": "2f05ccfe2458b98999262cca199a82162d4e8238490587cc016e0cdeb2987045", 
+    "s1", "deposit_message_root": "dmr", 
+    "deposit_data_root": "dd1", 
     "fork_version": "00000000", "network_name": "mainnet", "deposit_cli_version": "2.2.0"}, 
-    {"pubkey": "846e4d7e4d58407a9d9a8a1900e8fb72729fdf75f8fda3edb787921b50de45746765394c06f568e233978c52c04e77a8", 
-    "withdrawal_credentials": "0033e43b8d20ab691308d708fb88f2af58a5df82f26ecdf9e54ff5d5acc3cd95", 
+    {"pubkey": "pk2", 
+    "withdrawal_credentials": "wc2", 
     "amount": 32000000000, 
-    "signature": "a43b64d6c0ee176445a4bde7af42b0de982edce4a7d1e7b40708fb12206e4069a352a58c760cf13e53dc6e849fefae9b05528d5e1b1e4182e108474189bdc20f59fddedb4624315e67ba74cd74deed5023de7f97c2ef80cb4cc5cc995a074bc4", "deposit_message_root": "b1bc5324ee3d1653e95d4320877107274e0fc426371758acab49f26bca1c6b27", 
-    "deposit_data_root": "fb3dc81853fad137f534ca816a97a130e9b162cbaba4571e8f00fc1d96998f64", "fork_version": "00000000", "network_name": "mainnet", 
+    "signature": "s2", "deposit_message_root": "b1bc5324ee3d1653e95d4320877107274e0fc426371758acab49f26bca1c6b27", 
+    "deposit_data_root": "dd2", "fork_version": "00000000", "network_name": "mainnet", 
     "deposit_cli_version": "2.2.0"}
     ]`)
 
@@ -49,10 +49,19 @@ func main() {
 	//fmt.Println(len(deposit))
 
 	for i := range deposit {
-		_, err3 := f.WriteString(deposit[i].Pubkey)
+		f.WriteString(deposit[i].Pubkey)
 	}
-	if err3 != nil {
-		log.Fatal(err3)
+	f.WriteString("\n")
+	for i := range deposit {
+		f.WriteString(deposit[i].Withdrawal_credentials)
+	}
+	f.WriteString("\n")
+	for i := range deposit {
+		f.WriteString(deposit[i].Signature)
+	}
+	f.WriteString("\n")
+	for i := range deposit {
+		f.WriteString(deposit[i].Deposit_data_root)
 	}
 
 	fmt.Println("done")
